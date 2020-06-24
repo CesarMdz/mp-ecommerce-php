77bc95e3d7ed8a3dde7128b68bb496270bf71df5
CesarMdz <63685857+CesarMdz@users.noreply.github.com>
2020-06-24 18:12:46 -0500

    Update notificacion.php

--- respuesta/notificacion.php
+++ respuesta/notificacion.php
@@ -3,9 +3,14 @@ require '../vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken("TEST-2715478544766023-091119-711c045e5944e37e1443f14c75c16049-147706667");
 
+if (!isset($_POST["data_id"], $_POST["type"]) || !ctype_digit($_POST["data_id"])) {
+    http_response_code(400);
+    return;
+}
 switch($_POST["type"]) {
     case "payment":
         $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+        print_r($payment);
         return response('OK', 201);
         break;
     case "plan":

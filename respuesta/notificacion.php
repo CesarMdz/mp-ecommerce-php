9809f9b5804c7798cbb7454cf1f0ffab197fd524
CesarMdz <63685857+CesarMdz@users.noreply.github.com>
2020-06-24 18:39:22 -0500

    Update notificacion.php

--- respuesta/notificacion.php
+++ respuesta/notificacion.php
@@ -3,19 +3,48 @@ require '../vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken("TEST-2715478544766023-091119-711c045e5944e37e1443f14c75c16049-147706667");
 
-$payment = isset($_POST["payment_id"]) ? MercadoPago\Payment::find_by_id($_POST["payment_id"]) : NULL;
-$merchant_order = isset($_POST["merchant_order_id"]) ? MercadoPago\MerchantOrder::find_by_id($_POST["merchant_order_id"]) : NULL;
+switch($_POST["type"]) {
+            case "payment":
+                $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+                break;
+            case "plan":
+                $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+                break;
+            case "subscription":
+                $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+                break;
+            case "invoice":
+                $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+                break;
+        }
+        
+        if (isset($payment) || $payment != NULL){
+            $cart_id = explode('_', $payment['external_reference']);
 
-if($merchant_order === NULL){
-    if( isset($_POST["payment_id"]) ){
-        $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order_id);
-        print_r( $merchant_order);
-    }else{
-        echo 'Error datos faltantes!!';
-        die();
-    }
-}
+            $order = new Order((int)Order::getOrderByCartId((int)$cart_id[1]));
+            $current_state = (int)$order->getCurrentState();
 
+            switch($payment['status']){
+                case 'approved':
+                    $order_state ="PS_OS_PAYMENT";
+                    $mensaje="El Pago ha sido aprobado y acreditado";
+                    break;
+                case 'pending' || 'authorized' || 'in_process':
+                    $order_state ="MERCADOPAGO_PENDING";
+                    $mensaje ="El Pago se encuentra Pendiente de Validación";
+                    break;
+                case 'rejected':
+                     $order_state ="MERCADOPAGO_REJECTED";
+                    $mensaje ="El Pago ha sido rechazado.";
+                    break;
+                default:
+                    $order_state ="MERCADOPAGO_FAILED";
+                    $mensaje ="El proceso de pago ha fallado.";
+                    break;                        
+            }
+            if($current_state!=Configuration::get($order_state)) $order->setCurrentState((int)Configuration::get($order_state));
+        }
+        exit();  
 
 
 

e357532bef401f0bffbcd84aafb1b5c3cc782832
CesarMdz <63685857+CesarMdz@users.noreply.github.com>
2020-06-24 14:53:04 -0500

    Update notificacion.php

--- respuesta/notificacion.php
+++ respuesta/notificacion.php
@@ -6,15 +6,19 @@ MercadoPago\SDK::setAccessToken("TEST-2715478544766023-091119-711c045e5944e37e14
 switch($_POST["type"]) {
     case "payment":
         $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+        return response('OK', 201);
         break;
     case "plan":
         $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+        return response('OK', 201);
         break;
     case "subscription":
         $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+        return response('OK', 201);
         break;
     case "invoice":
         $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+        return response('OK', 201);
         break;
 }
 

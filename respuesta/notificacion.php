3c9f1bf15ee9d18c8dca2b59545559d0ff00ffce
CesarMdz <63685857+CesarMdz@users.noreply.github.com>
2020-06-25 14:31:35 -0500

    Update notificacion.php

--- respuesta/notificacion.php
+++ respuesta/notificacion.php
@@ -2,16 +2,44 @@
 
 require '../vendor/autoload.php';
 
-	MercadoPago\SDK::setAccessToken('APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948');
+MercadoPago\SDK::setAccessToken('APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948');
 
-   //   $id = isset($_POST["id"]) ? $_POST["id"] : null; 
-    //  $topic = isset($_POST["type"]) ? $_POST["type"] : null;
+  
 
-  if(isset($_GET['type'])){
-	  echo 'Se recibio algo';
-  }else{
-  	echo 'no se recibio nada';
-  }
+    $merchant_order = null;
+
+    switch($_GET["topic"]) {
+        case "payment":
+            $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
+		    console.log($payment);
+            // Get the payment and the corresponding merchant_order reported by the IPN.
+            $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
+		 console.log($merchant_order);
+            break;
+        case "merchant_order":
+            $merchant_order = MercadoPago\MerchantOrder::find_by_id($_GET["id"]);
+            break;
+    }
+
+    $paid_amount = 0;
+    foreach ($merchant_order->payments as $payment) {
+        if ($payment['status'] == 'approved'){
+            $paid_amount += $payment['transaction_amount'];
+        }
+    }
+
+    // If the payment's transaction amount is equal (or bigger) than the merchant_order's amount you can release your items
+    if($paid_amount >= $merchant_order->total_amount){
+        if (count($merchant_order->shipments)>0) { // The merchant_order has shipments
+            if($merchant_order->shipments[0]->status == "ready_to_ship") {
+                print_r("Totally paid. Print the label and release your item.");
+            }
+        } else { // The merchant_order don't has any shipments
+            print_r("Totally paid. Release your item.");
+        }
+    } else {
+        print_r("Not paid yet. Do not release your item.");
+    }
 ?>
 
 

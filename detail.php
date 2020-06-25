d76a1e23c5de4c2fbbe8b12789f0ecdf8ba73508
CesarMdz <63685857+CesarMdz@users.noreply.github.com>
2020-06-24 19:21:15 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -179,21 +179,20 @@ exit;
                                                      $preference->items = array($item);
                                                      
                                                      $payer = new MercadoPago\Payer();
-                                                     $payer->name = "Lalo";
-                                                     $payer->surname = "Landa";
-                                                     $payer->first_name="Lalo";
-                                                     $payer->last_name="Lalanda";
-                                                     $payer->email = "test_user_58295862@testuser.com";
-                                                     $payer->phone = array(
-                                                         "area_code" => "52",
-                                                         "number" => "5549737300"
-                                                       );
-                                                   
-                                                       $payer->address = array(
-                                                         "street_name" => "Insurgentes Sur",
-                                                         "street_number" => "1602",
-                                                         "zip_code" => "03940"
-                                                       );
+                                                      $payer->name = "Lalo";
+                                                      $payer->surname = "Landa";
+                                                      $payer->email = "test_user_58295862@testuser.com";
+                                                      $payer->date_created = "2020-06-02T18:58:41.425-04:00";
+                                                      $payer->phone = array(
+                                                        "area_code" => "52",
+                                                        "number" => "5549737300"
+                                                      );
+
+                                                      $payer->address = array(
+                                                        "street_name" => "Insurgentes Sur",
+                                                        "street_number" => 1602,
+                                                        "zip_code" => "03940"
+                                                      );
 
                                                        
                                                      

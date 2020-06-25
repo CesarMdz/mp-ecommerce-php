4198276b3b76dfb571d4378523b0005a59808b37
CesarMdz <63685857+CesarMdz@users.noreply.github.com>
2020-06-24 21:58:10 -0500

    Update notificacion.php

--- respuesta/notificacion.php
+++ respuesta/notificacion.php
@@ -1,10 +1,7 @@
 <?php
-$fichero = 'test.txt';
-// La nueva persona a añdir al fichero
-$persona = "John Smith\n";
-// Escribir los contenidos en el fichero,
-// usando la bandera FILE_APPEND para añadir el contenido al final del fichero
-// y la bandera LOCK_EX para evitar que cualquiera escriba en el fichero al mismo tiempo
-file_put_contents($fichero, $persona, FILE_APPEND | LOCK_EX);
+$myfile=fopen('test.txt','rt');
+flock($myfile,LOCK_SH);
+$read=file_get_contents('test.txt');
+fclose($myfile);
                 
 ?>

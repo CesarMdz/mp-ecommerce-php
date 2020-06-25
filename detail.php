fef038ffc39633ee979378eb746ef26ef56e5a13
CesarMdz <63685857+CesarMdz@users.noreply.github.com>
2020-06-25 18:21:48 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -148,7 +148,7 @@ exit;
                                                     
                                                      $preference = new MercadoPago\Preference();
                                                       $preference->external_reference = "cesar.mendez.hernandez@gmail.com";
-                                                      $preference->notification_url="https://cesarmdz-mp-commerce-php.herokuapp.com/webhooks.php";
+                                                      $preference->notification_url="https://enottzrxga2dp.x.pipedream.net";
                                                       $preference->back_urls = array(
                                                          "success" => "https://cesarmdz-mp-commerce-php.herokuapp.com/respuesta/success.php",
                                                          "failure" => "https://cesarmdz-mp-commerce-php.herokuapp.com/respuesta/failure.php",

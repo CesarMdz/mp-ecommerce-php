3162f462d4b1f28836fac1e09f9770211402c775
CesarMdz <63685857+CesarMdz@users.noreply.github.com>
2020-06-25 13:33:55 -0500

    Update notificacion.php

--- respuesta/notificacion.php
+++ respuesta/notificacion.php
@@ -4,8 +4,8 @@ require '../vendor/autoload.php';
 
 	MercadoPago\SDK::setAccessToken('APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948');
 
-      $id = isset($_POST["id"]) ? $_POST["id"] : null;
-$topic = isset($_POST["type"]) ? $_POST["type"] : null;
+   //   $id = isset($_POST["id"]) ? $_POST["id"] : null; 
+    //  $topic = isset($_POST["type"]) ? $_POST["type"] : null;
 
   if(isset($_POST['type'])){
 	  echo 'Se recibio algo';

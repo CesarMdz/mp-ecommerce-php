8bcae41b7b0f4c92fdb4ece62042ad5210ff51ee
CesarMdz <63685857+CesarMdz@users.noreply.github.com>
2020-06-25 00:35:36 -0500

    Update notificacion.php

--- respuesta/notificacion.php
+++ respuesta/notificacion.php
@@ -1,24 +1,83 @@
 <?php
-http_response_code(200);
 
-foreach ($_GET as $key => $value) {
-    $response .= htmlspecialchars($key)."=".htmlspecialchars($value)."&";
-}
+require 'vendor/autoload.php';
 
-$myfile = fopen("test.txt", "a");
-fwrite($myfile, date('m/d/Y h:i:s a', time()) . " " . $response . "|||" . file_get_contents("php://input"));
-fclose($myfile);
+	MercadoPago\SDK::setAccessToken('APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948');
 
+    switch($_POST["type"]) {
+		    
+        case "payment":
+		    echo $_POST['type'];
+            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+            write_json_log($payment,"./log/webhooksTest-PaymentStandard-".$id."-out-".date('Y-m-d').".json");
+            break;
+        case "plan":
+            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+            break;
+        case "subscription":
+            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+            break;
+        case "invoice":
+            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+            break;
+    }
 
-if ($_GET["topic"] == 'payment'){
+     
+    
 
-	$curl = "curl -X GET 'https://api.mercadopago.com/v1/payments/".$_GET["id"]."?access_token=APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'";
-  
-	$output = shell_exec($curl); 
+    function write_json_log($content, $file){
+    	
+        try {
+            //file_fix_directory(dirname($file));
+    
+            if (gettype($content) == 'string') {
+                $json = json_encode(array('data' => json_decode($content, true)), JSON_PRETTY_PRINT);
+                file_put_contents("php://stderr", $json);
+                if (file_put_contents($file, $json)) {
+                    return true;
+                }
+            } else {
+                $json = json_encode(array('data' => $content), JSON_PRETTY_PRINT);
+                file_put_contents("php://stderr", $json);
+                if (file_put_contents($file, $json)) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (Exception $e) {
+            write_general_log($content, $file);
+        }
+    }
 
-	$myfile = fopen("test.txt", "w");
-	fwrite($myfile, $output);
-	fclose($myfile);
+    function write_general_log($content, $file){
+    	
+        try {
+            $general = dirname($file) . "general.log";
+    
+            //file_fix_directory(dirname($file));
+    
+            if (gettype($content) == 'string') {
+                $json = json_encode(array('file' => $file, 'data' => json_decode($content, true)), JSON_PRETTY_PRINT);
+                //error_log($json);
+                file_put_contents("php://stderr", $json,FILE_APPEND);
+                if (file_put_contents($general, $json, LOCK_EX)) {
+                    return true;
+                }
+            } else {
+                $json = json_encode(array('file' => $file, 'data' => $content), JSON_PRETTY_PRINT);
+                //error_log($json);
+                file_put_contents("php://stderr", $json,FILE_APPEND);
+                if (file_put_contents($general, $json, LOCK_EX)) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (Exception $e) {
+            file_fix_directory(dirname($file));
+            echo($e->getMessage);
+        }
+    }
 
-}
 ?>
+
+
